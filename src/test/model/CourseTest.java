0e49449e1618da8fa053e34da06f6e52baa7055f
fanghan@students.cs.ubc.ca <fanghan@students.cs.ubc.ca>
2026-02-08 21:51:29 -0800

    Added tests for Course class

---
+++ src/test/model/CourseTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CourseTest {
+    private Course testCourse;
+
+    @BeforeEach
+    void setup() {
+        testCourse = new Course("CPSC210", 4, 98.0f);
+    }
+
+    @Test
+
+    void testConstructor() {
+        assertEquals("CPSC210", testCourse.getClass());
+        assertEquals(4, testCourse.getCredits());
+        assertEquals(98.0f, testCourse.getGrade());
+
+    }
+
+}
